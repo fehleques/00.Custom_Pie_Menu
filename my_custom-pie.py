53ba05481175d68675d94ffc3448b8fbe0b777ce
fehleques <117779898+fehleques@users.noreply.github.com>
2024-04-17 18:53:09 -0300

    Add custom pie menu for vertex actions in mesh edit mode

--- my_custom-pie.py
+++ my_custom-pie.py
@@ -10,6 +10,21 @@ bl_info = {
     "category": "Custom Pie Menu"
 }
 
+class VertexActions(bpy.types.Menu):
+    bl_label = "Vertex Actions"
+    bl_idname = "VIEW3D_MT_vertex_actions"
+
+    def draw(self, context):
+        layout = self.layout
+        pie = layout.menu_pie()
+
+        # Add single vertex
+        pie.operator("mesh.primitive_circle_add", text="Add Single Vertex", icon='VERTEXSEL').vertices = 1
+
+        # Toggle subdivision surface modifier in edit mode
+        # Use an operator to toggle the subdivision modifier visibility in edit mode.
+        pie.operator("object.modifier_toggle_viewport", text="Toggle Subdivision Surface", icon='MOD_SUBSURF').modifier = "Subdivision"
+
 # Define the Custom Pie Menu class
 class CustomPieMenu(bpy.types.Menu):
     bl_label = "Custom Pie Menu"
@@ -18,7 +33,7 @@ class CustomPieMenu(bpy.types.Menu):
     def draw(self, context):
         layout = self.layout
         pie = layout.menu_pie()
-        
+
         # Add cube operator
         pie.operator("mesh.primitive_cube_add", text="Add Cube", icon='MESH_CUBE')
     
@@ -29,11 +44,16 @@ class CustomPieMenu(bpy.types.Menu):
         if context.active_object and context.active_object.mode == 'EDIT' and context.active_object.type == 'MESH':
             pie.operator("mesh.merge", text="Merge at Last", icon='SNAP_VERTEX').type = 'LAST'
 
+            # Add vertex actions menu only in mesh edit mode
+            pie.menu("VIEW3D_MT_vertex_actions", text="Vertex Actions", icon='VERTEXSEL')
+
 # Register function
 def register():
     bpy.utils.register_class(CustomPieMenu)
+    bpy.utils.register_class(VertexActions)
+
+    # Register the keymap
     wm = bpy.context.window_manager
-    # Use the appropriate keymap for the 3D View
     km = wm.keyconfigs.addon.keymaps.new(name='3D View', space_type='VIEW_3D')
     kmi = km.keymap_items.new('wm.call_menu_pie', 'W', 'PRESS', alt=True)
     kmi.properties.name = "VIEW3D_MT_custom_pie_menu"
@@ -41,12 +61,15 @@ def register():
 # Unregister function
 def unregister():
     bpy.utils.unregister_class(CustomPieMenu)
-    wm = bpy.context.window_manager
-    km = wm.keyconfigs.addon.keymaps['3D View']
-    for kmi in km.keymap_items:
-        if kmi.properties.name == "VIEW3D_MT_custom_pie_menu":
-            km.keymap_items.remove(kmi)
+    bpy.utils.unregister_class(VertexActions)
 
-# Register the addon when the script is executed directly
+    # Unregister the keymap
+    wm = bpy.context.window_manager
+    km = wm.keyconfigs.addon.keymaps.get('3D View')
+    if km:
+        for kmi in km.keymap_items:
+            if kmi.properties.name == "VIEW3D_MT_custom_pie_menu":
+                km.keymap_items.remove(kmi)
+
 if __name__ == "__main__":
     register()
